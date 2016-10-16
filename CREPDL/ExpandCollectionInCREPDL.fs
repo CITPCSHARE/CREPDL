635a024d26422bb04631203b4c8fd753761b8c8f
MURATA Makoto <eb2m-mrt@asahi-net.or.jp>
2016-10-16 09:39:12 +0900

    Removed unnecessary modules and functions.

--- CREPDL/ExpandCollectionInCREPDL.fs
+++ CREPDL/ExpandCollectionInCREPDL.fs
@@ -3,16 +3,6 @@
 open System.Collections.Generic
 open ISO10646Collection
 
-let isDefinedInCREPDL (collectionNumber: int option) (collectionName: string option): bool  =
-
-    let numbers = List.map (fun (x, _, _) -> x) collectionsInCREPDL
-    let names = List.map (fun (_, x, _) -> x) collectionsInCREPDL
-
-    match (collectionName, collectionNumber) with
-           | Some(nm), _  -> List.contains nm names
-           | _, Some(cnum)  -> List.contains cnum numbers
-           | _,_ -> failwith "Specify either a name or a number"  
-
 
 let getCollectionInCREPDL  (collectionNumber: int option) (collectionName: string option) =
     
