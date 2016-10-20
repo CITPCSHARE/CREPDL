fb1becb9048d0f9a941d7f15d4cedbef9926bd86
MURATA Makoto <eb2m-mrt@asahi-net.or.jp>
2016-10-20 23:03:53 +0900

    Refactoring for Kyouiku Kanji.

--- CREPDLTest/Test.fs
+++ CREPDLTest/Test.fs
@@ -10,6 +10,7 @@ open CREPDL.ThreeValuedBoolean
 open CREPDL.ReadingCREPDL
 open CREPDL.TopLevelValidation
 open ConstructAllCollections
+open Kyouiku
 
 
 let dir = @"H:\CREPDLScripts\examples\"
@@ -53,22 +54,7 @@ validateString repCol (dir + "MalayalamE.crepdl") "abc" System.Console.Out |> wr
 
 System.Console.ReadKey() |> ignore;;
 
-System.Console.WriteLine "教育漢字一年生"
-validateString repCol (dir + "Kyouiku1a.crepdl") "一二三四五征悟父暗漁禁己" System.Console.Out |> writeThreeValuedBoolean
-validateString repCol (dir + "Kyouiku1b.crepdl")  "一二三四五征悟父暗漁禁己" System.Console.Out |> writeThreeValuedBoolean
-System.Console.WriteLine "教育漢字二年生"
-validateString repCol (dir + "Kyouiku2a.crepdl") "一二三四五征悟父暗漁禁己" System.Console.Out |> writeThreeValuedBoolean
-validateString repCol (dir + "Kyouiku2b.crepdl") "一二三四五征悟父暗漁禁己" System.Console.Out |> writeThreeValuedBoolean
-System.Console.WriteLine "教育漢字一二年生"
-validateString repCol (dir + "Kyouiku1-2.crepdl") "一二三四五征悟父暗漁禁己" System.Console.Out |> writeThreeValuedBoolean
-System.Console.WriteLine "教育漢字一二三年生"
-validateString repCol (dir + "Kyouiku1-3.crepdl") "一二三四五征悟父暗漁禁己" System.Console.Out |> writeThreeValuedBoolean
-System.Console.WriteLine "教育漢字一二三四年生"
-validateString repCol (dir + "Kyouiku1-4.crepdl") "一二三四五征悟父暗漁禁己" System.Console.Out |> writeThreeValuedBoolean
-System.Console.WriteLine "教育漢字一二三四五年生"
-validateString repCol (dir + "Kyouiku1-5.crepdl") "一二三四五征悟父暗漁禁己" System.Console.Out |> writeThreeValuedBoolean
-System.Console.WriteLine "教育漢字一二三四五六年生"
-validateString repCol (dir + "Kyouiku1-6.crepdl") "一二三四五征悟父暗漁禁己" System.Console.Out |> writeThreeValuedBoolean
+testKyouiku repCol dir
 
 System.Console.ReadKey() |> ignore;;
 
