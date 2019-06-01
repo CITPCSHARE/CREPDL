3934d5015f23871d63bf5eec227faf9771f7a611
MURATA Makoto <eb2m-mrt@asahi-net.or.jp>
2019-06-01 09:57:08 +0900

    Rewrite based on the EPUBValidation project.

--- EPUBConsoleApplication/Program.fs
+++ EPUBConsoleApplication/Program.fs
@@ -1,74 +1,43 @@
 ﻿open System
-open HtmlAgilityPack
-open CREPDL.Validation
+open CREPDL
 open System.IO
-open System.IO.Compression
-open System.Web
+open CREPDL.EPUBValidation
 
-let crepdlSource = 
-        @"<union xmlns=""http://purl.oclc.org/dsdl/crepdl/ns/structure/2.0"" mode=""graphemeCluster"">
-        <repertoire 
-          registry=""10646"" number=""371""/> <!-- JIS2004 IDEOGRAPHICS EXTENSION -->
-        <repertoire 
-          registry=""10646"" number=""285""/> <!-- BASIC JAPANESE (or JIS X 0208) -->
-        <repertoire 
-          registry=""10646"" number=""286""/> <!-- JAPANESE NON IDEOGRAPHICS EXTENSION -->
-        <repertoire 
-          registry=""10646"" number=""287""/> <!--  COMMON JAPANESE -->
-        <char>[\n|\r|\t]|(\r\n)</char>
-        </union>"
-
-let printCharacter (str: string) =
-    printf "%s (" str
-    for i = 0 to str.Length - 1 do
-            let v = str.Chars i |> int
-            v.ToString("x4") |>
-                if i = str.Length - 1 then
-                 printf "U+%s" 
-                else printf "%s, "
-    printfn ")"
-
-let scanZip (filePath: string) (validator: Validator) = 
-    let zipFile = ZipFile.OpenRead(filePath)
-    printfn "UnZipping"
-    for entry in zipFile.Entries do
-        if entry.Name.EndsWith(".xhtml") 
-            || entry.Name.EndsWith(".html") 
-            || entry.Name.EndsWith(".htm") then
-
-            let mutable unknowns = set []
-            let mutable notIncluded = set []
-            let htmlDocument = new HtmlDocument()
-            htmlDocument.Load(entry.Open(), System.Text.Encoding.UTF8)
-            printfn "Checking File name: %s" entry.FullName
-            for node in htmlDocument.DocumentNode.SelectNodes("//*/text()") do
-                let decodedText = HttpUtility.HtmlDecode( node.InnerText)
-                let (unknowns1, notIncluded1) = validator.validateTextStream(new StringReader(decodedText))
-                unknowns <- Set.union unknowns (Set.ofArray(unknowns1))
-                notIncluded <- Set.union notIncluded (Set.ofArray(notIncluded1))
-            if unknowns.Count <> 0 then
-                System.Console.WriteLine("Unknowns:")
-            for u in unknowns do
-                printCharacter u
-            if notIncluded.Count <> 0 then
-                System.Console.WriteLine("Not Included:")
-            for n in notIncluded do
-                printCharacter n
-
+let private crepdlSource = 
+    @"<union xmlns=""http://purl.oclc.org/dsdl/crepdl/ns/structure/2.0"" mode=""graphemeCluster"">
+    <repertoire 
+      registry=""10646"" number=""371""/> <!-- JIS2004 IDEOGRAPHICS EXTENSION -->
+    <repertoire 
+      registry=""10646"" number=""285""/> <!-- BASIC JAPANESE (or JIS X 0208) -->
+    <repertoire 
+      registry=""10646"" number=""286""/> <!-- JAPANESE NON IDEOGRAPHICS EXTENSION -->
+    <repertoire 
+      registry=""10646"" number=""287""/> <!--  COMMON JAPANESE -->
+    <char>[\n|\r|\t]|(\r\n)</char>
+    </union>"
 
 [<EntryPoint>]
 let main argv = 
     Console.OutputEncoding <- System.Text.Encoding.UTF8;
+    use sw = new StreamWriter(Console.OpenStandardOutput())
     match argv with 
-    | [|epubFileName; crepdlFileName |] -> 
+    | [| crepdlFileName; epubFileName |] -> 
         printfn "Started"
-        let validator = new Validator(new StreamReader(crepdlFileName))
-        printfn "Validator created"
-        scanZip epubFileName validator
+        try
+            let validator = new CREPDLValidator(new StreamReader(crepdlFileName))
+            printfn "Validator created"
+            try 
+              scanZip sw epubFileName validator
+            with
+            | :? SystemException -> 
+              Console.Error.WriteLine("Error in the EPUB file.")
+        with 
+        | :? System.Xml.XmlException -> 
+            Console.Error.WriteLine("Syntax error in the CREPDL Script")
     | [|epubFileName|] -> 
         printfn "Started"
-        let validator = new Validator(new StringReader(crepdlSource))
+        let validator = new CREPDLValidator(new StringReader(crepdlSource))
         printfn "Validator created"
-        scanZip epubFileName validator
-    | _ -> Console.WriteLine("Specify an EPUB file name")
+        scanZip sw epubFileName validator
+    | _ -> Console.WriteLine("Specify an EPUB file name or a CREPDL script file name followed by an EPUB file name")
     0 // return an integer exit code
